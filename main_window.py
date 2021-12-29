b723e7fa04c4cf778211d393ec8e58d1df0f788a
mremond <matthieu.remond@sfr.fr>
2021-12-29 09:36:05 +0100

    gitignore

--- main_window.py
+++ main_window.py
@@ -1,24 +1,26 @@
 from PyQt5 import QtWidgets
 from table_model import table_model
 
+height = 600
+width = 1000 
+
+def generate_empty_data():
+    return [[0 for i in range(9)] for j in range(9)]
+
 class main_window(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.create_table_view()
 
-        self.setCentralWidget(self._table)
+        self.setMinimumSize(width, height)
+        self.setWindowTitle("PySudoku")
+        self.setCentralWidget(self._table_view)
 
     def create_table_view(self):
-        data = [
-            [4, 9, 2],
-            [1, 0, 0],
-            [3, 5, 0],
-            [3, 3, 2],
-            [7, 8, 9],
-        ]
+        data = generate_empty_data()
 
-        self._table = QtWidgets.QTableView()
+        self._table_view = QtWidgets.QTableView()
         self._table_model = table_model(data)
 
-        self._table.setModel(self._table_model)
+        self._table_view.setModel(self._table_model)
