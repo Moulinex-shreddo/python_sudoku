a6df059d741d6ab750fc394b6b32a1880b960732
mremond <matthieu.remond@sfr.fr>
2022-01-07 10:19:24 +0100

    fix unit tests

--- libs/test_sudoku_solver.py
+++ libs/test_sudoku_solver.py
@@ -6,10 +6,12 @@ dummy_matrix[0][0] = 1
 
 # Returns True if all sudoku_solver unit tests are PASS, False, otherwise
 def test_sudoku_solver():
-    b = (test_valid_number()
-    and test_invalid_row()
-    and test_invalid_col()
-    and test_invalid_block())
+    b = True
+
+    b &= test_valid_number()
+    b &= test_invalid_row()
+    b &= test_invalid_col()
+    b &= test_invalid_block()
 
     if b:
         print("test_sudoku_solver : PASS")
@@ -21,44 +23,44 @@ def test_sudoku_solver():
 
 # Returns True if we can add a valid number in the cell, False otherwise
 def test_valid_number():
-    b = is_number_valid(dummy_matrix, 1, (1, 1))
+    b = is_number_valid(dummy_matrix, 5, (1, 1))
 
     if b:
         print("test_sudoku_solver/valid_number : PASS")
     else:
         print("test_sudoku_solver/valid_number : FAIL")
-    
+
     return b
 
 # Returns True if we fail to add an invalid number in a row, False otherwise
 def test_invalid_row():
-    b = is_number_valid(dummy_matrix, 1, (0, 10))
+    b = not is_number_valid(dummy_matrix, 1, (0, 5))
 
-    if not b:
+    if b:
         print("test_sudoku_solver/invalid_row : PASS")
     else:
         print("test_sudoku_solver/invalid_row : FAIL")
-    
-    return not b
+
+    return b
 
 # Returns True if we fail to add an invalid number in a column, False otherwise
 def test_invalid_col():
-    b = is_number_valid(dummy_matrix, 1, (10, 0))
+    b = not is_number_valid(dummy_matrix, 1, (5, 0))
 
-    if not b:
+    if b:
         print("test_sudoku_solver/invalid_col : PASS")
     else:
         print("test_sudoku_solver/invalid_col : FAIL")
 
-    return not b
+    return b
 
 # Returns True if we fail to add an invalid number in a block, False otherwise
 def test_invalid_block():
-    b = is_number_valid(dummy_matrix, 1, (1, 1))
+    b = not is_number_valid(dummy_matrix, 1, (1, 1))
 
-    if not b:
+    if b:
         print("test_sudoku_solver/invalid_block : PASS")
     else:
         print("test_sudoku_solver/invalid_block : FAIL")
-    
-    return not b
+
+    return b
